ed5039e1a5476882ce9f5b5b11a6d73df27f2644
vasylboliuk <vbboliuk@gmail.com>
2017-01-29 20:31:50 +0200

    Add new solution String1: First two characters in string

---
+++ src/String1/FirstTwo.java
@@ -0,0 +1,23 @@
+package String1;
+
+/**
+ * Created by Dell on 1/29/2017.
+ */
+public class FirstTwo {
+
+    /**
+     * Given a string, return the string made of its first two chars, so the String "Hello" yields "He".
+     * If the string is shorter than length 2, return whatever there is, so "X" yields "X",
+     * and the empty string "" yields the empty string "". Note that str.length() returns the length of a string.
+     *
+     * firstTwo("Hello") → "He"
+     * firstTwo("abcdefg") → "ab"
+     * firstTwo("ab") → "ab"
+     */
+    public String firstTwo(String str) {
+        if (str.length()<2){
+            return str;
+        }
+        return str.substring(0,2);
+    }
+}
