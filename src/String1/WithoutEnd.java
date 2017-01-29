6f65195d521d7e4a2c7d8e51f6a994b882506c0a
vasylboliuk <vbboliuk@gmail.com>
2017-01-29 21:14:35 +0200

    Add new solution String1: Without end

---
+++ src/String1/WithoutEnd.java
@@ -0,0 +1,20 @@
+package String1;
+
+/**
+ * Created by Dell on 1/29/2017.
+ */
+public class WithoutEnd {
+
+    /**
+     * Given a string, return a version without the first and last char, so "Hello" yields "ell".
+     * The string length will be at least 2.
+     *
+     * withoutEnd("Hello") → "ell"
+     * withoutEnd("java") → "av"
+     * withoutEnd("coding") → "odin"
+     */
+    public String withoutEnd(String str) {
+
+        return str.substring(1,str.length()-1);
+    }
+}
