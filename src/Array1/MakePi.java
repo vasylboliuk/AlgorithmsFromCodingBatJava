9fcb1720b50639441845be28fa2cef6a46f1a5be
vasylboliuk <vbboliuk@gmail.com>
2017-02-02 20:43:00 +0200

    Add new solution Array-1: MakePi

---
+++ src/Array1/MakePi.java
@@ -0,0 +1,18 @@
+package Array1;
+
+/**
+ * Created by Dell on 2/2/2017.
+ */
+public class MakePi {
+
+    /**
+     * Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+     *
+     * makePi() → [3, 1, 4]
+     * @return
+     */
+    public int[] makePi() {
+        int[] pi = {3, 1, 4};
+        return pi;
+    }
+}
