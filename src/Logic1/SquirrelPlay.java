cd2d0366f18a63c4459d8e2dbffd738735c849e1
vasylboliuk <vbboliuk@gmail.com>
2017-02-02 17:02:25 +0200

    Add new solution Logic1: SquirrelPlay

---
+++ src/Logic1/SquirrelPlay.java
@@ -0,0 +1,21 @@
+package Logic1;
+
+/**
+ * Created by vboliuk on 2/2/2017.
+ */
+public class SquirrelPlay {
+    /**
+     * The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature
+     * is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90.
+     * Given an int temperature and a boolean isSummer, return true if the squirrels play and false otherwise.
+     *
+     * squirrelPlay(70, false) → true
+     * squirrelPlay(95, false) → false
+     * squirrelPlay(95, true) → true
+     */
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if(temp >= 60 && temp <= 90) return true;
+        else if(temp >= 90 && temp <= 100 && isSummer) return true;
+        else return false;
+    }
+}
