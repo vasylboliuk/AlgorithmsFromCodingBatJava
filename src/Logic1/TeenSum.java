d7aaaa51e21ec86e79500e7ef16d50a3d3fbc1d7
vasylboliuk <vbboliuk@gmail.com>
2017-02-02 17:19:33 +0200

    Add new solution Logic1: TeenSum

---
+++ src/Logic1/TeenSum.java
@@ -0,0 +1,21 @@
+package Logic1;
+
+/**
+ * Created by vboliuk on 2/2/2017.
+ */
+public class TeenSum {
+
+    /**
+     * Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive,
+     * are extra lucky. So if either value is a teen, just return 19.
+     *
+     * teenSum(3, 4) → 7
+     * teenSum(10, 13) → 19
+     * teenSum(13, 2) → 19
+     */
+    public int teenSum(int a, int b) {
+        int sum = a + b;
+        if((a>=13 && a<=19) || (b>=13 && b<=19)) return 19;
+        return sum;
+    }
+}
